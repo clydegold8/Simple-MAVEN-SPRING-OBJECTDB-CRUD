c2d1106d900ef586ec52b97bc8d3893adb650d13
Clyde Ian Acaso <ian.clyde24@gmail.com>
2016-07-19 11:33:58 +0800

    Added addtional files

--- target/generated-sources/annotations/com/simplecrud/NewMember_.java
+++ target/generated-sources/annotations/com/simplecrud/NewMember_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.1.v20130918-rNA", date="2016-07-19T10:26:55")
+@Generated(value="EclipseLink-2.5.1.v20130918-rNA", date="2016-07-19T11:30:16")
 @StaticMetamodel(NewMember.class)
 public class NewMember_ { 
 
