c98f57ebee5eee23181b644148df2255f7d421fa
Clyde Ian Acaso <ian.clyde24@gmail.com>
2016-07-20 10:18:28 +0800

    Added Additional Test Files :hammer:

---
+++ src/test/java/com/simplecrud/NewMemberTest.java
@@ -0,0 +1,118 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.simplecrud;
+
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author VIZI-BILL PH
+ */
+public class NewMemberTest extends TestCase {
+
+    public NewMemberTest(String testName) {
+        super(testName);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    /**
+     * Test of getId method, of class NewMember.
+     */
+    public void testGetId() {
+        System.out.println("getId");
+        NewMember instance = new NewMember();
+        instance.setId(1L);
+        Long expResult = 1L;
+        Long result = instance.getId();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of setId method, of class NewMember.
+     */
+    public void testSetId() {
+        System.out.println("setId");
+        Long id = 1L;
+        NewMember instance = new NewMember();
+        instance.setId(id);
+    }
+
+    /**
+     * Test of getUsername method, of class NewMember.
+     */
+    public void testGetUsername() {
+        System.out.println("getUsername");
+        NewMember instance = new NewMember();
+        instance.setUsername("test");
+        String expResult = "test";
+        String result = instance.getUsername();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of setUsername method, of class NewMember.
+     */
+    public void testSetUsername() {
+        System.out.println("setUsername");
+        String username = "test";
+        NewMember instance = new NewMember();
+        instance.setUsername(username);
+    }
+
+    /**
+     * Test of getPassword method, of class NewMember.
+     */
+    public void testGetPassword() {
+        System.out.println("getPassword");
+        NewMember instance = new NewMember();
+        instance.setPassword("test");
+        String expResult = "test";
+        String result = instance.getPassword();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of setPassword method, of class NewMember.
+     */
+    public void testSetPassword() {
+        System.out.println("setPassword");
+        String password = "test";
+        NewMember instance = new NewMember();
+        instance.setPassword(password);
+    }
+
+    /**
+     * Test of getEmail method, of class NewMember.
+     */
+    public void testGetEmail() {
+        System.out.println("getEmail");
+        NewMember instance = new NewMember();
+        instance.setEmail("test");
+        String expResult = "test";
+        String result = instance.getEmail();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of setEmail method, of class NewMember.
+     */
+    public void testSetEmail() {
+        System.out.println("setEmail");
+        String email = "test";
+        NewMember instance = new NewMember();
+        instance.setEmail(email);
+    }
+
+}
